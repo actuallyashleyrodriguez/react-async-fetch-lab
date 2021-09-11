23bfb4ebc584c80ac593167db95bcfbc6a32e9c7
Arodriguez1030 <arodriguez1030@gmail.com>
2021-09-10 23:08:38 -0400

    commit tree 1f56d398c0fc3d9c968b3c04acad28213f6e7285

--- src/App.js
+++ src/App.js
@@ -1,1 +1,19 @@
-// create your App component here
+import React, { Component } from "react";
+
+// create your App componen
+export default class App extends Component {
+    state = {
+        people: []
+    };
+    componentDidMount() {
+        fetch("http://api.open-notify.org/astros.json")
+        .then(results => results.json())
+        .then(data => this.setState({people: data.people}))
+    }
+    render() {
+        return (
+            <div>{this.state.people.map(person => person.name)}</div>
+        )
+
+    }
+}
